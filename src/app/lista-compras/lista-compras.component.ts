0566668206a12ddb77628162975a3674c5ba1f39
ThiagoFranca <francasantos.t@gmail.com>
2025-05-28 20:02:58 -0400

    finalizado

--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemLista } from './itemlista';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compras',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, CommonModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
+
+
 export class ListaComprasComponent {
+  item: string = '';
+  lista : ItemLista[] = [];
+
+  adicionarItem(){
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+
+    this.item = '';
+
+    console.table(this.lista);
+  }
+
+  riscarItem(itemLista: ItemLista){
+    itemLista.comprado = !itemLista.comprado;
+  }
+
+  limparLista(){
+    this.lista = [];
+  }
 
 }
